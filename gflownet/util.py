5083b714281c2f80fd6623c905e45bc54b5b3886
Karl Ryden <karl.ryden@ri.se>
2025-04-11 15:33:22 +0200

    Bug fix.

--- gflownet/util.py
+++ gflownet/util.py
@@ -161,6 +161,7 @@ class TransitionBuffer(object):
             zip(*transition_ls)  # s_batch is a list of tensors
         gbatch = dgl.batch(gbatch)
 
+        cbatch = torch.stack(cbatch, dim=0)
         s_batch = torch.cat(s_batch, dim=0)  # (sum of # nodes in batch, )
         s_next_batch = torch.cat(s_next_batch, dim=0)
 
