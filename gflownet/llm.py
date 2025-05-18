a7940dba81ed7ae0b2e0de56dfb7b97bf6fde006
Karl Ryden <karl.ryden@ri.se>
2025-05-18 22:03:34 +0200

    Stores node identifiers.

--- gflownet/llm.py
+++ gflownet/llm.py
@@ -120,7 +120,12 @@ def embed_constraints(cfg):
                     x = pickle.load(p)
 
                 g = x['graph']
-                nx.set_node_attributes(g, {i: node_encodings[i].cpu().float() for i in range(len(g))}, 'encoding')
+                nx.set_node_attributes(g,
+                    {i: "this is node {i}" for i in range(len(g))},
+                    'identifier')
+                nx.set_node_attributes(g, 
+                    {i: node_encodings[i].cpu().float() for i in range(len(g))}, 
+                    'encoding')
 
                 x['embedding'] = c.cpu().float()
 
